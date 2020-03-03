2bf2f9618fc4e9753e78cd8d1041ae7ce9c3d805
Saif <shaq1@sheffield.ac.uk>
2020-03-03 15:23:23 +0000

    Added test and started embeddings

--- tf_wordembeddings.py
+++ tf_wordembeddings.py
@@ -4,4 +4,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 
+embedding_layer = layers.Embedding(1000, 5)
 
+result = embedding_layer(tf.constant([1,2,3]))
+result.numpy()
