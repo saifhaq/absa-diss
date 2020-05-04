33be3f001b0f489b83a9985894903f23c20f51db
Saif <saif@archlon.localdomain>
2020-05-04 14:02:08 +0100

    data_df populated with all three models

--- main_system/something.py
+++ main_system/something.py
@@ -17,7 +17,10 @@ import pandas as pd
 
 # train_df = pd.read_pickle(path.join('pandas_data', 'aspect_category_detection_train_'+str(16)+'_classes.pkl'))
 # df = pd.read_pickle(path.join('baseline', path.join('aspect', 'aspect_embedding_layer.pkl')))
-df = pd.read_pickle(path.join('main_system', path.join('aspect', 'aspects_glove.pkl')))
+# df = pd.read_pickle(path.join('main_system', path.join('aspect', 'aspects_glove.pkl')))
+df = pd.read_pickle(path.join('acd', 'data_df_.pkl'))
+
+
 # data_df2 = pd.read_pickle(path.join('main_system', path.join('aspect', 'aspect_embedding_layer.pkl')))
 
 
