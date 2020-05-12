30a0f5b59272c8cb55ccc2597ef4ecbf01d89fee
Saif <saif@archlon.localdomain>
2020-05-12 15:04:34 +0100

    Updated readme

--- acd_old/results/init_data_f1s.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os.path as path
-import xml.etree.ElementTree as et
-from collections import Counter
-
-data_f1s = pd.DataFrame(columns = ["model", "acc", "f1"])
-
-print(data_f1s)
-data_f1s.to_pickle(path.join('acd', path.join('results', 'data_f1s.pkl')))
